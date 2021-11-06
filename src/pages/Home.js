a6f15b0f63977c7fe0813310eafa05946dd27358
gouravmarch20 <gouravmarch20@gmail.com>
2021-11-07 00:15:07 +0530

    refactor

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,21 +7,33 @@ import GameDetail from '../components/GameDetail'
 import Game from '../components/Game'
 //Styling and Animation
 import styled from 'styled-components'
-// import { motion, AnimatePresence, AnimateSharedLayout } from 'framer-motion'
-// import { useLocation } from 'react-router-dom'
+import { motion } from "framer-motion"
+import { useLocation } from 'react-router-dom'
 // import { fadeIn } from '../animations'
 const Home = () => {
   //get the current location
   //   const location = useLocation()
 
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
   useEffect(() => {
-    dispatch(loadGames());
-  }, [dispatch]);
-//   const { popular } = useSelector(
-//     state => state.games
-//   )
-  return <div></div>
-}
+    dispatch(loadGames())
+  }, [dispatch])
+  // acess state
+  const { popular, newGames, upcoming } = useSelector(state => state.games)
 
+  // console.log(popular)
+  return (
+    <div>
+    <h1>hit</h1>
+    <h1>hit</h1>
+      {/* <GameList></GameList> */}
+    </div>
+  )
+}
+// const GameList = styled(motion.div)`
+//   padding: 0rem 5rem;
+//   h2 {
+//     padding: 5rem 0rem;
+//   }
+// `
 export default Home
